fa2886c539f3bbf40dc6325f4fafb57997c9ef88
Rajani <rajani.kami004@gmail.com>
2026-02-04 19:33:12 -0500

    Add bill input code

--- Lab2_Rabarail-#1.py
+++ Lab2_Rabarail-#1.py
@@ -3,5 +3,6 @@ AUthor: Rajani Baraili
 Purpose: Calculate 15% and 20% tip suggestions and totals for dinner bill.
 Starter code: None
 Date:02/04/2026"""
-
+print("Please, enter the total bill amount:")
+bill = float(input())
 
